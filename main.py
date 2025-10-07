f2a7dcb64c4b2a59af327c67c6c4a79a447b1104
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-10-07 19:29:54 +0200

    added cursor smoothing. cant test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
-import time
+import sys
 import math
+import loess
 import mediapipe as mp
 import numpy as np
 import pyautogui
@@ -43,30 +44,9 @@ class Landmarker():
     def close(self):
         self.landmarker.close()
 
-def draw_landmarks(rgb_image, detection_result: mp.tasks.vision.HandLandmarkerResult):
-   """Courtesy of https://github.com/googlesamples/mediapipe/blob/main/examples/hand_landmarker/python/hand_landmarker.ipynb"""
-   try:
-        if detection_result.hand_landmarks == []:
-            return rgb_image
-        else:
-            hand_landmarks_list = detection_result.hand_landmarks
-            annotated_image = np.copy(rgb_image)
-
-            for idx in range(len(hand_landmarks_list)):
-                hand_landmarks = hand_landmarks_list[idx]
-                
-                hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-                hand_landmarks_proto.landmark.extend([
-                landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks])
-                mp.solutions.drawing_utils.draw_landmarks(
-                annotated_image,
-                hand_landmarks_proto,
-                mp.solutions.hands.HAND_CONNECTIONS,
-                mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
-                mp.solutions.drawing_styles.get_default_hand_connections_style())
-            return annotated_image
-   except:
-        return rgb_image
+def map_coordinate(coord, old_min=0.2, old_max=0.8):
+    coord = max(old_min, min(old_max, coord))
+    return (coord - old_min) / (old_max - old_min)
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -74,17 +54,23 @@ def main():
     monitor = get_monitors()[0]
 
     hand_landmarker = Landmarker()
-
     mouse_down = False
+    
+    x_array = []
+    y_array = []
 
     while True:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        height, width, _ = frame.shape
+        try:
+            height, width, _ = frame.shape
+        except AttributeError:
+            print("No camera detected.")
+            sys.exit(1)
+            
         monitor_width, monitor_height = monitor.width, monitor.height
         
         hand_landmarker.detect_async(frame)
-        #frame = draw_landmarks(frame, hand_landmarker.result)
         try:
             if hand_landmarker.result and hand_landmarker.result.hand_landmarks and len(hand_landmarker.result.hand_landmarks[0]) == 21:
                 pointer_tip = hand_landmarker.result.hand_landmarks[0][8]
@@ -100,10 +86,14 @@ def main():
                     mouse_down = False
                 
                 norm_x, norm_y = pointer_tip.x, pointer_tip.y
+                mapped_x = map_coordinate(pointer_tip.x)
+                mapped_y = map_coordinate(pointer_tip.y)
                 pixel_x = int(norm_x * width)
                 pixel_y = int(norm_y * height)
-                monitor_x = int(norm_x * monitor_width)
-                monitor_y = int(norm_y * monitor_height)
+                monitor_x = int(mapped_x * monitor_width)
+                monitor_y = int(mapped_y * monitor_height)
+                x_array.append(monitor_x)
+                y_array.append(monitor_y)
                 cv2.circle(frame, (pixel_x, pixel_y), 10, (0, 255, 0), -1)
                 
                 thumb_pixel_x = int(thumb_tip.x * width)
@@ -112,6 +102,13 @@ def main():
                 
                 cv2.line(frame, (pixel_x, pixel_y), (thumb_pixel_x, thumb_pixel_y), (0, 0, 255), 2)
                 
+                if len(x_array) > 30:
+                    recent_x = x_array[-min(1000, len(x_array)):]
+                    recent_y = y_array[-min(1000, len(y_array)):]
+                    x_smooth = loess.loess_1d(np.arange(len(recent_x)), np.array(recent_x), frac=0.1)
+                    y_smooth = loess.loess_1d(np.arange(len(recent_y)), np.array(recent_y), frac=0.1)
+                    monitor_x = int(x_smooth[-1])
+                    monitor_y = int(y_smooth[-1])
                 pyautogui.moveTo(monitor_x, monitor_y)
         except (AttributeError, IndexError):
             pass
